d10761dd8061500cbc476318855acb4118baf56d
michael1011 <me@michael1011.at>
2019-02-24 15:20:26 +0100

    feat: add Litecoin LNDs to regtest image

--- docker/regtest/scripts/start.sh
+++ docker/regtest/scripts/start.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
+source utils.sh
+
+function waitForLnd () {
+  while true; do
+    if $1 getinfo 2>&1 | grep version > /dev/null 2>&1; then
+      break
+    fi
+    sleep 1
+  done
+}
+
 bitcoind
 litecoind
 
 # Wait for Bitcoin and Litecoin Core to start
-while true; do
-	if bitcoin-cli getblockchaininfo 2>&1 | grep blocks > /dev/null 2>&1; then
-		break	
-	fi
-	sleep 1
-done
-
-while true; do
-	if litecoin-cli getblockchaininfo 2>&1 | grep blocks > /dev/null 2>&1; then
-		break	
-	fi
-	sleep 1
-done
+waitForNode bitcoin-cli
+waitForNode litecoin-cli
 
 echo "Starting LNDs"
 
@@ -24,19 +24,14 @@ echo "Starting LNDs"
 nohup lnd --lnddir=/root/.lnd-btc --listen=127.0.0.1:9735 --rpclisten=0.0.0.0:10009 --restlisten=0.0.0.0:8080 --bitcoin.active --bitcoin.regtest --bitcoin.node=bitcoind > /dev/null 2>&1 & num="0"
 nohup lnd --lnddir=/root/.lnd-btc2 --listen=127.0.0.1:9736 --rpclisten=0.0.0.0:10010 --restlisten=0.0.0.0:8081 --bitcoin.active --bitcoin.regtest --bitcoin.node=bitcoind > /dev/null 2>&1 & num="0"
 
+nohup lnd --lnddir=/root/.lnd-ltc --listen=127.0.0.1:10735 --rpclisten=0.0.0.0:11009 --restlisten=0.0.0.0:9080 --litecoin.active --litecoin.regtest --litecoin.node=litecoind --litecoind.rpchost=127.0.0.1:19443 > /dev/null 2>&1 & num="0"
+nohup lnd --lnddir=/root/.lnd-ltc2 --listen=127.0.0.1:10736 --rpclisten=0.0.0.0:11010 --restlisten=0.0.0.0:9081 --litecoin.active --litecoin.regtest --litecoin.node=litecoind --litecoind.rpchost=127.0.0.1:19443 > /dev/null 2>&1 & num="0"
+
 # Wait for the LNDs to start
-while true; do
-  if lncli --lnddir=/root/.lnd-btc --rpcserver=127.0.0.1:10009 --network=regtest getinfo 2>&1 | grep version > /dev/null 2>&1; then
-    break
-  fi
-  sleep 1
-done
+waitForLnd "lncli --lnddir=/root/.lnd-btc --rpcserver=127.0.0.1:10009 --network=regtest"
+waitForLnd "lncli --lnddir=/root/.lnd-btc2 --rpcserver=127.0.0.1:10010 --network=regtest"
 
-while true; do
-  if lncli --lnddir=/root/.lnd-btc2 --rpcserver=127.0.0.1:10010 --network=regtest getinfo 2>&1 | grep version > /dev/null 2>&1; then
-    break
-  fi
-  sleep 1
-done
+waitForLnd "lncli --lnddir=/root/.lnd-ltc --rpcserver=127.0.0.1:11009 --chain=litecoin --network=regtest"
+waitForLnd "lncli --lnddir=/root/.lnd-ltc2 --rpcserver=127.0.0.1:11010 --chain=litecoin --network=regtest"
 
-echo "Started nodes"
+echo "Started LNDs"
---
+++ docker/regtest/scripts/utils.sh
@@ -0,0 +1,8 @@
+function waitForNode () {
+  while true; do
+    if $1 getblockchaininfo 2>&1 | grep blocks > /dev/null 2>&1; then
+      break	
+    fi
+    sleep 1
+  done  
+}

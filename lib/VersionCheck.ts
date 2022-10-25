7b999d79d241af4b433c283849db15f9e40f9989
michael1011 <me@michael1011.at>
2022-10-25 12:43:06 +0200

    chore: set max LND version to 0.15.3

--- lib/VersionCheck.ts
+++ lib/VersionCheck.ts
@@ -8,7 +8,7 @@ class VersionCheck {
 
   private static lndVersionLimits = {
     minimal: '0.12.0',
-    maximal: '0.15.2',
+    maximal: '0.15.3',
   };
 
   public static checkChainClientVersion = (symbol: string, version: number): void => {

5698f29ca02db1de7e53051ca7f0ae49175a9a64
michael1011 <me@michael1011.at>
2022-11-01 19:21:09 +0100

    chore: update LND to v0.15.4

--- docker/build.py
+++ docker/build.py
@@ -37,7 +37,7 @@ GETH_VERSION = "1.10.25"
 
 C_LIGHTNING_VERSION = "0.12.1"
 ECLAIR_VERSION = "0.7.0-patch-disconnect"
-LND_VERSION = "0.15.3-beta"
+LND_VERSION = "0.15.4-beta"
 
 BITCOIN_BUILD_ARG = BuildArgument(
     name="BITCOIN_VERSION",
@@ -89,7 +89,7 @@ IMAGES: Dict[str, Image] = {
         arguments=[],
     ),
     "regtest": Image(
-        tags=["3.4.3"],
+        tags=["3.4.4"],
         arguments=[
             UBUNTU_VERSION,
             BITCOIN_BUILD_ARG,

256bb392ce012368071172cc5476488998ecbec6
michael1011 <me@michael1011.at>
2024-06-26 15:13:51 +0200

    chore: bump LND version to v0.18.1-beta

--- docker/regtest/startRegtest.sh
+++ docker/regtest/startRegtest.sh
@@ -28,7 +28,7 @@ docker run \
   -p 9735:9735 \
   -p 9293:9293 \
   -p 9292:9292 \
-  boltz/regtest:4.5.2
+  boltz/regtest:4.5.3
 
 docker exec regtest bash -c "cp /root/.lightning/regtest/*.pem /root/.lightning/regtest/certs"
 docker exec regtest chmod -R 777 /root/.lightning/regtest/certs

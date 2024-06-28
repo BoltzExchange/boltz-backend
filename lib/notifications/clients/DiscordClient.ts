dbafa201d1369ea821f4fecc9bb95cff9a609833
michael1011 <me@michael1011.at>
2024-06-28 15:19:28 +0200

    fix: Mattermost max message length

--- lib/notifications/clients/DiscordClient.ts
+++ lib/notifications/clients/DiscordClient.ts
@@ -2,29 +2,25 @@ import { GatewayIntentBits } from 'discord-api-types/v10';
 import { Client, Message, TextChannel } from 'discord.js';
 import { NotificationConfig } from '../../Config';
 import Logger from '../../Logger';
-import { codeBlock } from '../Markup';
 import NotificationClient from './NotificationClient';
 
-class DiscordClient extends NotificationClient {
+class DiscordClient extends NotificationClient<Client, TextChannel> {
   public static readonly serviceName = 'Discord';
 
-  private static readonly maxMessageLen = 2000;
-
-  private readonly client: Client;
-
-  private channel?: TextChannel = undefined;
-  private channelAlerts?: TextChannel = undefined;
-
   constructor(logger: Logger, config: NotificationConfig) {
-    super(DiscordClient.serviceName, logger, config);
-
-    this.client = new Client({
-      intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-      ],
-    });
+    super(
+      DiscordClient.serviceName,
+      logger,
+      config,
+      new Client({
+        intents: [
+          GatewayIntentBits.Guilds,
+          GatewayIntentBits.GuildMessages,
+          GatewayIntentBits.MessageContent,
+        ],
+      }),
+      2000,
+    );
   }
 
   public init = async (): Promise<void> => {
@@ -78,47 +74,8 @@ class DiscordClient extends NotificationClient {
     }
   };
 
-  public sendMessage = async (
-    message: string,
-    isAlert?: boolean,
-  ): Promise<void> => {
-    const channel = this.selectChannel(
-      this.channel,
-      this.channelAlerts,
-      isAlert,
-    );
-
-    if (channel === undefined) {
-      return;
-    }
-
-    if (message.length + this.prefix.length <= DiscordClient.maxMessageLen) {
-      await channel.send(`${this.prefix}${message}`);
-    } else {
-      let toSplit = message;
-      let maxPartLen = DiscordClient.maxMessageLen;
-
-      const isCodeBlock =
-        message.startsWith(codeBlock) && message.endsWith(codeBlock);
-
-      if (isCodeBlock) {
-        // When splitting code blocks we need to account for the length the code block markup needs
-        maxPartLen -= codeBlock.length * 2;
-
-        // Trim the code block markup from the original message that will be split
-        toSplit = toSplit.substring(
-          codeBlock.length,
-          toSplit.length - codeBlock.length,
-        );
-      }
-
-      await channel.send(this.prefix);
-
-      for (const part of this.splitString(toSplit, maxPartLen)) {
-        const getBlockMarkup = () => (isCodeBlock ? codeBlock : '');
-        await channel.send(`${getBlockMarkup()}${part}${getBlockMarkup()}`);
-      }
-    }
+  protected sendRawMessage = async (channel: TextChannel, message: string) => {
+    await channel.send(message);
   };
 
   private listenForMessages = () => {
@@ -135,11 +92,6 @@ class DiscordClient extends NotificationClient {
     this.client.on('error', (error) => {
       this.emit('error', error);
     });
-  };
-
-  private splitString = (toSplit: string, length: number): string[] => {
-    const splitRegex = new RegExp(`[\\s\\S]{1,${length}}`, 'g');
-    return toSplit.match(splitRegex) || [];
   };
 }
 

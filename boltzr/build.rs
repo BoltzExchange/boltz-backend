d2d123645445cc97a43b7a148a432fb3e0e3ad54
michael1011 <me@michael1011.at>
2024-08-21 03:23:47 +0200

    chore: include libpq-dev in CLN image

--- boltzr/build.rs
+++ boltzr/build.rs
@@ -2,7 +2,7 @@ fn main() {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile(&["protos/boltzr.proto"], &["protos"])
-        .unwrap_or_else(|e| panic!("Could build protos: {}", e));
+        .unwrap_or_else(|e| panic!("Could not build protos: {}", e));
     built::write_built_file()
         .unwrap_or_else(|e| panic!("Failed to acquire build-time information: {}", e));
 }

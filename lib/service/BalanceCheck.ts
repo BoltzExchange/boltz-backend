ce479fd1eb148bc90822c552b5e87a076c016199
michael1011 <me@michael1011.at>
2024-09-10 17:12:50 +0200

    feat: check balance before creating swap (#663)

---
+++ lib/service/BalanceCheck.ts
@@ -0,0 +1,20 @@
+import WalletManager from '../wallet/WalletManager';
+import Errors from './Errors';
+
+class BalanceCheck {
+  constructor(private readonly walletManager: WalletManager) {}
+
+  public checkBalance = async (symbol: string, amount: number) => {
+    const wallet = this.walletManager.wallets.get(symbol);
+    if (wallet === undefined) {
+      throw Errors.CURRENCY_NOT_FOUND(symbol);
+    }
+
+    const balance = await wallet.getBalance();
+    if (balance.confirmedBalance < amount) {
+      throw Errors.INSUFFICIENT_LIQUIDITY();
+    }
+  };
+}
+
+export default BalanceCheck;

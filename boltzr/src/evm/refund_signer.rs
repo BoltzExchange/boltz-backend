453c68d013ab446b7fab7c4e9f7d867a2549742c
michael1011 <me@michael1011.at>
2025-02-07 19:53:50 +0100

    feat: WebSocket inactivity timeout

--- boltzr/src/evm/refund_signer.rs
+++ boltzr/src/evm/refund_signer.rs
@@ -178,7 +178,7 @@ pub mod test {
         let contract = EtherSwap::new(ETHER_SWAP_ADDRESS.parse().unwrap(), provider.clone());
 
         let mut preimage_hash = FixedBytes::<32>::default();
-        rand::thread_rng().fill(&mut preimage_hash[..]);
+        rand::rng().fill(&mut preimage_hash[..]);
 
         let amount = U256::from(1);
         let timelock: u64 = 1;
@@ -241,7 +241,7 @@ pub mod test {
         let contract = ERC20Swap::new(ERC20_SWAP_ADDRESS.parse().unwrap(), provider.clone());
 
         let mut preimage_hash = FixedBytes::<32>::default();
-        rand::thread_rng().fill(&mut preimage_hash[..]);
+        rand::rng().fill(&mut preimage_hash[..]);
 
         let amount = U256::from(1);
         let timelock: u64 = 1;

8a8e45ce573afce44f155729cdd0324801716169
michael1011 <me@michael1011.at>
2025-03-11 12:55:42 +0100

    fix: skip mempool acceptance check on regtest (#848)

--- lib/chain/elements/ZeroConfCheck.ts
+++ lib/chain/elements/ZeroConfCheck.ts
@@ -3,5 +3,6 @@ import { Transaction } from 'liquidjs-lib';
 export interface ZeroConfCheck {
   get name(): string;
 
+  init(): Promise<void>;
   checkTransaction(transaction: Transaction): Promise<boolean>;
 }

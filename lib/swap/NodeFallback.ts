06966c4850fe8f1b34c01804d2f34d8f61d3850e
maybeast <78227110+maybeast@users.noreply.github.com>
2025-04-02 14:49:57 -0600

    chore: bump max invoice description length to 500 (#876)

--- lib/swap/NodeFallback.ts
+++ lib/swap/NodeFallback.ts
@@ -19,8 +19,8 @@ type HolisticInvoice = InvoiceWithRoutingHints & {
 class NodeFallback {
   private static readonly addInvoiceTimeout = 10_000;
   private static readonly invoiceMemoRegex = new RegExp(
-    // Visible ASCII characters with a maximal length of 100
-    '^[\x20-\x7E]{0,100}$',
+    // Visible ASCII characters with a maximal length of 500
+    '^[\x20-\x7E]{0,500}$',
   );
 
   constructor(

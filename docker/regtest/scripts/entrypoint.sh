47b68215f94c52403b123eae8a31dfe53ecf66b8
michael1011 <me@michael1011.at>
2025-04-04 16:14:42 +0200

    feat: ARK swaps

--- docker/regtest/scripts/entrypoint.sh
+++ docker/regtest/scripts/entrypoint.sh
@@ -11,6 +11,27 @@ elements-cli rescanblockchain 0 > /dev/null
 startClns
 startLnds
 
+startEsplora
+
+mkdir -p /cookies
+cp /root/.bitcoin/regtest/.cookie /cookies/.bitcoin-cookie
+cp /root/.elements/liquidregtest/.cookie /cookies/.elements-cookie
+
+chmod 777 /cookies/.*
+
+startAsp
+sleep 10
+bitcoin-cli -generate 1 > /dev/null
+sleep 10
+arkd wallet unlock --password ark > /dev/null
+
+startFulmine
+sleep 20
+curl -s -X POST http://localhost:7001/api/v1/wallet/unlock \
+  -H "Content-Type: application/json" \
+  -d '{"password": "ark"}' > /dev/null
+
 while true; do
   sleep 1
 done
+

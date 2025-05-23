f4ffc284246536a70a798afd591a63db5972f5a6
michael1011 <me@michael1011.at>
2025-05-23 19:36:47 +0200

    chore: bump hold to v0.3.1 (#948)

--- lib/VersionCheck.ts
+++ lib/VersionCheck.ts
@@ -86,7 +86,7 @@ class VersionCheck {
     },
     [ClnClient.serviceNameHold]: {
       minimal: '0.3.0',
-      maximal: '0.3.0',
+      maximal: '0.3.1',
     },
     [LndClient.serviceName]: {
       minimal: '0.18.0',

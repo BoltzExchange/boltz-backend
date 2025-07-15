5a351315ec9a75d693bfcc650e73701dd7689699
michael1011 <me@michael1011.at>
2025-07-15 17:58:05 +0200

    fix: Ark transactions preimage detection

--- lib/swap/SwapManager.ts
+++ lib/swap/SwapManager.ts
@@ -1384,7 +1384,7 @@ class SwapManager {
         (swap.status === SwapUpdateEvent.TransactionMempool ||
           swap.status === SwapUpdateEvent.TransactionConfirmed)
       ) {
-        const { chainClient } = this.currencies.get(chainCurrency)!;
+        const { chainClient, arkNode } = this.currencies.get(chainCurrency)!;
 
         if (chainClient) {
           const transactionId = reverseBuffer(
@@ -1399,6 +1399,13 @@ class SwapManager {
               wallet.decodeAddress(swap.lockupAddress),
             );
           }
+        } else if (arkNode) {
+          arkNode.subscribeAddresses([
+            {
+              address: swap.lockupAddress,
+              preimageHash: getHexBuffer(swap.preimageHash),
+            },
+          ]);
         }
       } else {
         const { arkNode, chainClient } = this.currencies.get(chainCurrency)!;

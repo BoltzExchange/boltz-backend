1881e03cc83835f56d3c41a5d13dfb29da4b18b3
maybeast <78227110+maybeast@users.noreply.github.com>
2025-07-23 16:13:28 +0300

    feat: swap update trigger

---
+++ boltzr/migrations/2025-07-23-115549_swap_update_trigger/up.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION check_invoice_update()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF OLD.invoice IS NOT NULL AND OLD.invoice != NEW.invoice THEN
+        RAISE EXCEPTION 'INVOICE_ALREADY_SET';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_check_invoice_update
+BEFORE UPDATE ON swaps
+FOR EACH ROW EXECUTE FUNCTION check_invoice_update();

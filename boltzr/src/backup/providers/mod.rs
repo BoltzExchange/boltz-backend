c667dc60948a693f16a06eabd040c76d8844f109
michael1011 <me@michael1011.at>
2025-09-02 16:54:07 +0200

    refactor: backup provider names

--- boltzr/src/backup/providers/mod.rs
+++ boltzr/src/backup/providers/mod.rs
@@ -6,6 +6,8 @@ pub mod s3;
 
 #[async_trait]
 pub trait BackupProvider: std::fmt::Debug {
+    fn name(&self) -> String;
+
     async fn put(&self, path: &str, data: &[u8]) -> anyhow::Result<()>;
 
     async fn put_stream(

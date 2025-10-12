bc1aed98846fb43ad1ab43b08bb4ee2f82d52fe1
michael1011 <me@michael1011.at>
2025-10-12 15:11:32 +0200

    chore: bump Bitcoin Core to v30.0

--- lib/VersionCheck.ts
+++ lib/VersionCheck.ts
@@ -78,7 +78,7 @@ class VersionCheck {
   > = {
     [ChainClient.serviceName]: {
       minimal: 230000,
-      maximal: 290100,
+      maximal: 300000,
     },
     [ClnClient.serviceName]: {
       minimal: '25.05',

2e10c85fa33a508af6544f6d631ebdddc956c53d
michael1011 <me@michael1011.at>
2025-12-19 16:50:16 +0100

    refactor: LND gRPC connector implementation

--- boltzr/build.rs
+++ boltzr/build.rs
@@ -6,6 +6,11 @@ fn main() {
 
     tonic_prost_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(&["../proto/lnd/rpc.proto"], &["../proto"])
+        .unwrap_or_else(|e| panic!("Could not build LND protos: {e}"));
+
+    tonic_prost_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(&["../proto/cln/node.proto"], &["../proto"])
         .unwrap_or_else(|e| panic!("Could not build CLN protos: {e}"));
 
--- boltzr/src/lightning/lnd/mod.rs
+++ boltzr/src/lightning/lnd/mod.rs
@@ -1,15 +1,41 @@
 use crate::chain::BaseClient;
 use anyhow::anyhow;
 use async_trait::async_trait;
-use fedimint_tonic_lnd::lnrpc::{
-    ChanBackupExportRequest, ChannelBackupSubscription, GetInfoRequest,
-};
+use lnd_rpc::{ChanBackupExportRequest, ChannelBackupSubscription, GetInfoRequest};
 use serde::{Deserialize, Serialize};
+use std::str::FromStr;
 use std::time::Duration;
 use tokio_util::sync::CancellationToken;
+use tonic::metadata::MetadataValue;
+use tonic::service::interceptor::InterceptedService;
 use tracing::{debug, error, info, instrument, warn};
 
 const RECONNECT_INTERVAL: Duration = Duration::from_secs(5);
+
+#[allow(
+    dead_code,
+    clippy::enum_variant_names,
+    clippy::large_enum_variant,
+    clippy::doc_lazy_continuation
+)]
+mod lnd_rpc {
+    tonic::include_proto!("lnrpc");
+}
+
+#[derive(Clone)]
+struct MacaroonInterceptor {
+    macaroon: MetadataValue<tonic::metadata::Ascii>,
+}
+
+impl tonic::service::Interceptor for MacaroonInterceptor {
+    fn call(&mut self, request: tonic::Request<()>) -> Result<tonic::Request<()>, tonic::Status> {
+        let mut request = request;
+        request
+            .metadata_mut()
+            .insert("macaroon", self.macaroon.clone());
+        Ok(request)
+    }
+}
 
 #[derive(Deserialize, Serialize, PartialEq, Clone, Debug)]
 pub struct Config {
@@ -20,12 +46,22 @@ pub struct Config {
     pub macaroonpath: String,
 }
 
+type LndClient = lnd_rpc::lightning_client::LightningClient<
+    tonic::service::interceptor::InterceptedService<
+        hyper_util::client::legacy::Client<
+            hyper_rustls::HttpsConnector<hyper_util::client::legacy::connect::HttpConnector>,
+            tonic::body::Body,
+        >,
+        MacaroonInterceptor,
+    >,
+>;
+
 #[derive(Clone)]
 pub struct Lnd {
     cancellation_token: CancellationToken,
 
     symbol: String,
-    lnd: fedimint_tonic_lnd::Client,
+    lnd: LndClient,
 
     scb_backup_tx: tokio::sync::broadcast::Sender<Vec<u8>>,
 }
@@ -39,23 +75,60 @@ impl Lnd {
     ) -> anyhow::Result<Self> {
         let (scb_backup_tx, _) = tokio::sync::broadcast::channel::<Vec<u8>>(16);
 
+        let tls_config = tls::config(config.certpath).await?;
+
+        let mut http = hyper_util::client::legacy::connect::HttpConnector::new();
+        http.enforce_http(false);
+
+        let address = format!("https://{}:{}", config.host, config.port);
+
+        let connector = tower::ServiceBuilder::new()
+            .layer_fn(move |s| {
+                let tls_config = tls_config.clone();
+
+                hyper_rustls::HttpsConnectorBuilder::new()
+                    .with_tls_config(tls_config)
+                    .https_or_http()
+                    .enable_http2()
+                    .wrap_connector(s)
+            })
+            .service(http);
+
+        let client =
+            hyper_util::client::legacy::Client::builder(hyper_util::rt::TokioExecutor::new())
+                .build(connector);
+
+        let intercepted = InterceptedService::new(
+            client,
+            MacaroonInterceptor {
+                macaroon: MetadataValue::from_str(&alloy::hex::encode(
+                    tokio::fs::read(&config.macaroonpath).await.map_err(|e| {
+                        anyhow::anyhow!(
+                            "failed to read macaroon from {}: {}",
+                            config.macaroonpath,
+                            e
+                        )
+                    })?,
+                ))?,
+            },
+        );
+
+        let lnd = lnd_rpc::lightning_client::LightningClient::with_origin(
+            intercepted,
+            tonic::transport::Uri::from_str(&address)?,
+        );
+
         Ok(Lnd {
-            scb_backup_tx,
             cancellation_token,
             symbol: symbol.to_string(),
-            lnd: fedimint_tonic_lnd::connect(
-                format!("https://{}:{}", config.host, config.port),
-                config.certpath,
-                config.macaroonpath,
-            )
-            .await?,
+            lnd,
+            scb_backup_tx,
         })
     }
 
     pub async fn channel_backup(&mut self) -> anyhow::Result<Option<Vec<u8>>> {
         let res = self
             .lnd
-            .lightning()
             .export_all_channel_backups(ChanBackupExportRequest {})
             .await?
             .into_inner();
@@ -73,7 +146,6 @@ impl Lnd {
     async fn start_listeners(&mut self) -> anyhow::Result<()> {
         let mut backup_stream = self
             .lnd
-            .lightning()
             .subscribe_channel_backups(ChannelBackupSubscription {})
             .await?
             .into_inner();
@@ -113,12 +185,7 @@ impl BaseClient for Lnd {
     }
 
     async fn connect(&mut self) -> anyhow::Result<()> {
-        let info = self
-            .lnd
-            .lightning()
-            .get_info(GetInfoRequest {})
-            .await?
-            .into_inner();
+        let info = self.lnd.get_info(GetInfoRequest {}).await?.into_inner();
         info!(
             "Connected to {} LND {} ({})",
             self.symbol,
@@ -148,5 +215,113 @@ impl BaseClient for Lnd {
         });
 
         Ok(())
+    }
+}
+
+mod tls {
+    use anyhow::Result;
+    use rustls::client::ClientConfig;
+    use rustls::client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier};
+    use rustls::crypto::{CryptoProvider, WebPkiSupportedAlgorithms};
+    use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
+    use rustls::{DigitallySignedStruct, Error as TLSError, SignatureScheme};
+    use std::{
+        path::{Path, PathBuf},
+        sync::Arc,
+    };
+
+    pub(crate) async fn config(path: impl AsRef<Path> + Into<PathBuf>) -> Result<ClientConfig> {
+        Ok(ClientConfig::builder()
+            .dangerous()
+            .with_custom_certificate_verifier(Arc::new(CertVerifier::load(path).await?))
+            .with_no_client_auth())
+    }
+
+    #[derive(Debug)]
+    pub(crate) struct CertVerifier {
+        certs: Vec<Vec<u8>>,
+        supported_algs: WebPkiSupportedAlgorithms,
+    }
+
+    impl CertVerifier {
+        pub(crate) async fn load(path: impl AsRef<Path> + Into<PathBuf>) -> Result<Self> {
+            let contents = tokio::fs::read(&path)
+                .await
+                .map_err(|e| anyhow::anyhow!("failed to read certificate file: {}", e))?;
+            let cert_der_vec =
+                rustls_pemfile::certs(&mut &*contents).collect::<Result<Vec<_>, _>>()?;
+            let certs: Vec<Vec<u8>> = cert_der_vec
+                .into_iter()
+                .map(|cert_der| cert_der.to_vec())
+                .collect();
+
+            let provider = CryptoProvider::get_default()
+                .ok_or_else(|| anyhow::anyhow!("must install default crypto provider"))?;
+
+            Ok(CertVerifier {
+                certs,
+                supported_algs: provider.signature_verification_algorithms,
+            })
+        }
+    }
+
+    impl ServerCertVerifier for CertVerifier {
+        fn verify_server_cert(
+            &self,
+            end_entity: &CertificateDer,
+            intermediates: &[CertificateDer],
+            _server_name: &ServerName,
+            _ocsp_response: &[u8],
+            _now: UnixTime,
+        ) -> Result<ServerCertVerified, TLSError> {
+            let mut certs = intermediates
+                .iter()
+                .map(|c| c.as_ref().to_vec())
+                .collect::<Vec<Vec<u8>>>();
+            certs.push(end_entity.as_ref().to_vec());
+            certs.sort();
+
+            if self.certs.len() != certs.len() {
+                return Err(TLSError::General(format!(
+                    "mismatched number of certificates (expected: {}, presented: {})",
+                    self.certs.len(),
+                    certs.len()
+                )));
+            }
+
+            let mut our_certs = self.certs.clone();
+            our_certs.sort();
+
+            for (c, p) in our_certs.iter().zip(certs.iter()) {
+                if *p != *c {
+                    return Err(TLSError::General(
+                        "server certificates do not match ours".to_string(),
+                    ));
+                }
+            }
+            Ok(ServerCertVerified::assertion())
+        }
+
+        fn verify_tls12_signature(
+            &self,
+            message: &[u8],
+            cert: &CertificateDer,
+            dss: &DigitallySignedStruct,
+        ) -> Result<HandshakeSignatureValid, TLSError> {
+            rustls::crypto::verify_tls12_signature(message, cert, dss, &self.supported_algs)
+        }
+
+        fn verify_tls13_signature(
+            &self,
+            message: &[u8],
+            cert: &CertificateDer,
+            dss: &DigitallySignedStruct,
+        ) -> Result<HandshakeSignatureValid, TLSError> {
+            rustls::crypto::verify_tls13_signature(message, cert, dss, &self.supported_algs)
+        }
+
+        fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
+            self.supported_algs.supported_schemes()
+        }
     }
 }

cb393ba75da5a227b58dd87a3acf54461c7209f3
jackstar12 <jkranawetter05@gmail.com>
2026-02-04 13:57:15 -0600

    chore: sql formatting

--- boltzr/migrations/2025-12-02-104258-0000_funding_address/down.sql
+++ boltzr/migrations/2025-12-02-104258-0000_funding_address/down.sql
@@ -1,9 +1,22 @@
 -- This file should undo anything in `up.sql`
 DROP TRIGGER IF EXISTS update_script_pubkeys_modified_time ON script_pubkeys;
-ALTER TABLE script_pubkeys ALTER COLUMN "createdAt" DROP DEFAULT;
-ALTER TABLE script_pubkeys ALTER COLUMN "updatedAt" DROP DEFAULT;
-ALTER TABLE script_pubkeys ALTER COLUMN swap_id SET NOT NULL;
-ALTER TABLE script_pubkeys DROP COLUMN IF EXISTS funding_address_id;
-ALTER TABLE script_pubkeys DROP CONSTRAINT IF EXISTS script_pubkeys_exclusive_id;
+
+ALTER TABLE script_pubkeys
+ALTER COLUMN "createdAt"
+DROP DEFAULT;
+
+ALTER TABLE script_pubkeys
+ALTER COLUMN "updatedAt"
+DROP DEFAULT;
+
+ALTER TABLE script_pubkeys
+ALTER COLUMN swap_id
+SET NOT NULL;
+
+ALTER TABLE script_pubkeys
+DROP COLUMN IF EXISTS funding_address_id;
+
+ALTER TABLE script_pubkeys
+DROP CONSTRAINT IF EXISTS script_pubkeys_exclusive_id;
 
 DROP TABLE IF EXISTS funding_addresses;

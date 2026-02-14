65179da758ecd4fe7d59ce5582d26ddf03c7ef38
michael1011 <me@michael1011.at>
2026-02-14 20:15:54 +0100

    chore: bump LND to v0.20.1

--- docker/build.py
+++ docker/build.py
@@ -37,7 +37,7 @@ NODE_VERSION = BuildArgument(
 
 GOLANG_VERSION = BuildArgument(
     name="GOLANG_VERSION",
-    value="1.25.5-trixie",
+    value="1.25.7-trixie",
 )
 
 BITCOIN_VERSION = "30.2"
@@ -47,7 +47,7 @@ GETH_VERSION = "1.16.7"
 
 C_LIGHTNING_VERSION = "25.12.1"
 ECLAIR_VERSION = "0.13.1"
-LND_VERSION = "0.20.0-beta"
+LND_VERSION = "0.20.1-beta"
 
 BITCOIN_BUILD_ARG = BuildArgument(
     name="BITCOIN_VERSION",

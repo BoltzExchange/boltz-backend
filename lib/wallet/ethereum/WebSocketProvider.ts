621ed062dc2c6918e63e56943759a3f09990a3aa
jackstar12 <j@bol.tz>
2026-06-09 13:11:01 +0200

    chore: revert listener logs

--- lib/wallet/ethereum/WebSocketProvider.ts
+++ lib/wallet/ethereum/WebSocketProvider.ts
@@ -221,9 +221,6 @@ class WebSocketProvider extends EthersWebSocketProvider {
     this.registeredListeners.get(event)!.add(listener);
 
     await super.on(event, listener);
-    this.logger.debug(
-      `${this.symbol} WebSocket ${this.name} registered listener for ${this.formatProviderEvent(event)} (${this.countRegisteredListeners(this.registeredListeners)} persistent, ${this.countRegisteredListeners(this.registeredOnceListeners)} once)`,
-    );
     return this;
   }
 
@@ -244,9 +241,6 @@ class WebSocketProvider extends EthersWebSocketProvider {
     this.onceWrappedListeners.set(listener, wrappedListener);
 
     await super.once(event, wrappedListener);
-    this.logger.debug(
-      `${this.symbol} WebSocket ${this.name} registered once listener for ${this.formatProviderEvent(event)} (${this.countRegisteredListeners(this.registeredListeners)} persistent, ${this.countRegisteredListeners(this.registeredOnceListeners)} once)`,
-    );
     return this;
   }
 
@@ -314,17 +308,6 @@ class WebSocketProvider extends EthersWebSocketProvider {
   }
 
   private reregisterListeners = async (): Promise<void> => {
-    const persistentListeners = this.countRegisteredListeners(
-      this.registeredListeners,
-    );
-    const onceListeners = this.countRegisteredListeners(
-      this.registeredOnceListeners,
-    );
-
-    this.logger.debug(
-      `${this.symbol} WebSocket ${this.name} re-registering ${persistentListeners} persistent listener(s) and ${onceListeners} once listener(s) after reconnect`,
-    );
-
     await super.removeAllListeners();
 
     for (const [event, listenerSet] of this.registeredListeners) {
@@ -347,31 +330,6 @@ class WebSocketProvider extends EthersWebSocketProvider {
     }
 
     this.resume();
-    this.logger.debug(
-      `${this.symbol} WebSocket ${this.name} re-registered ${persistentListeners} persistent listener(s) and ${onceListeners} once listener(s) after reconnect`,
-    );
-  };
-
-  private countRegisteredListeners = (
-    listeners: Map<ProviderEvent, Set<Listener>>,
-  ) =>
-    Array.from(listeners.values()).reduce(
-      (sum, listenerSet) => sum + listenerSet.size,
-      0,
-    );
-
-  private formatProviderEvent = (event: ProviderEvent): string => {
-    if (typeof event === 'string') {
-      return event;
-    }
-
-    try {
-      return JSON.stringify(event, (_, value) =>
-        typeof value === 'bigint' ? value.toString() : value,
-      );
-    } catch {
-      return String(event);
-    }
   };
 }
 
